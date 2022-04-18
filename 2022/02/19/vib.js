93d376bf1a9ebaf45ae055cd0f8f9dc2cc19cefa
Thysbelon <dnferch@gmail.com>
2022-04-18 18:23:13 -0400

    About page and Portfolio

--- 2022/02/19/vib.js
+++ 2022/02/19/vib.js
@@ -9,5 +9,5 @@ vid.addEventListener("ended",function(){vid.addEventListener("click",function(){
 if (navigator.userAgent.search("Edg") < navigator.userAgent.search("Chrome")) {
 	document.querySelector("#vibspeech img").src='vibout.avif'
 } else if (navigator.userAgent.search("Safari")===-1 || navigator.userAgent.search("Firefox")+navigator.userAgent.search("Android")===-2) {
-	document.querySelector("#vibspeech img").outerHTML='<video autoplay muted loop title="I drew this using Shake Art DELUXE by nokoi"><source src="vib-min.webm" type="video/webm">An image of Vibri\'s head with a speech bubble.</video>'
+	document.querySelector("#vibspeech img").outerHTML='<video autoplay muted loop playsinline title="I drew this using Shake Art DELUXE by nokoi"><source src="vib-min.webm" type="video/webm">An image of Vibri\'s head with a speech bubble.</video>'
 }
