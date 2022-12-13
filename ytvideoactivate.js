996721e5ca293700b8a8f376c8472e32f58b9ab3
Thysbelon <Thysbelon@users.noreply.github.com>
2022-12-13 05:43:16 +0000

    deploy: c0b747ddacb35a80bc776e0741fd14c41ad8182e

--- ytvideoactivate.js
+++ ytvideoactivate.js
@@ -7,12 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
 		for (let ytvid of ytvideos) {
 			let title=ytvid.innerText
 			console.log(title)
-			let videoid=ytvid.href.replace(/\/([\w-]*$)/ ,'$1')
+			let videoid=ytvid.href.replace(/.*\/([\w-]*$)/ ,'$1')
 			console.log(videoid)
 			let vidstart=ytvid.href.replace(/https:\/\/youtu\.be\/.*\?t=(\d*)/ ,'$1')
 			console.log(vidstart)
 			if (vidstart.includes("youtu.be")===false && ytvid.href.includes("youtube.com/clip")===false) {vidstart="&start="+vidstart} else {vidstart=""}
-			ytvid.outerHTML="<iframe src=https://www.youtube-nocookie.com/embed/"+videoid+"?playsinline=1"+vidstart+" title='Youtube video: "+title+"' allow=fullscreen></iframe>"
+			if (ytvid.href.includes("youtube.com/clip/")===true){ytvid.outerHTML="<iframe src=https://www.youtube.com/embed/"+ytvid.dataset.fullvidurl+"?clip="+videoid+"&clipt="+ytvid.dataset.clipt+"&playsinline=1 title='Youtube video: "+title+"' allow=fullscreen></iframe>"} // clips break when using nocookie. fun fact: a data- attribute can have any capitalization, but the equivalent js dataset *property* will always be all lowercase!
+			else {ytvid.outerHTML="<iframe src=https://www.youtube-nocookie.com/embed/"+videoid+"?playsinline=1"+vidstart+" title='Youtube video: "+title+"' allow=fullscreen></iframe>"}
 		}
 	}
 });
