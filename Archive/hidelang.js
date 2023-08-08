160857fadff94382911cc239ffdaede083c983d2
Thysbelon <Thysbelon@users.noreply.github.com>
2023-08-08 01:13:27 +0000

    deploy: 1a48fd2d7ffcec55b350686440bdfd06bda2d3b8

--- Archive/hidelang.js
+++ Archive/hidelang.js
@@ -1,9 +1,17 @@
 let prefl=navigator.language.slice(0,2);
 let lang='ja';
 if (prefl==='ja') {lang='en'};
-document.addEventListener('DOMContentLoaded', function() {
+function hidelang() {
 	let x=document.querySelectorAll(":lang("+lang+")");
 	for (let i=0, l=x.length; i < l; i++) {
 		x[i].style.display="none";
 	}
-})
+}
+
+if (document.readyState === "loading") {
+	// Loading hasn't finished yet
+	document.addEventListener("DOMContentLoaded", hidelang);
+} else {
+	// `DOMContentLoaded` has already fired
+	hidelang();
+}
