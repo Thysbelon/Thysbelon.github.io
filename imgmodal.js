9b3c81bc32a2db02d44bf22316da57aa4e55d35f
Thysbelon <dnferch@gmail.com>
2023-09-04 16:31:11 -0400

    Countdown Music Mod Post, Drawcia Digital Art

--- imgmodal.js
+++ imgmodal.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+	//if () {return}
 	let elementsArray = document.querySelectorAll(".gallery img, section > img");
 	document.querySelector("nav").outerHTML+="<dialog id=imgmodal><img></dialog>"
 	var modal = document.getElementById("imgmodal");
 	var modalImg = document.querySelector("#imgmodal img");
 	modal.addEventListener("click",function(){
+		modal.style.removeProperty('display')
 		modal.close()
 	})
 	elementsArray.forEach(function(elem) {
@@ -12,8 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
 			modalImg.src = this.src;
 			if (this.className.includes("pixl")) {
 				modal.style.imageRendering="pixelated"
-			} else {modal.style.imageRendering="auto"}
+				if (this.style.getPropertyValue("--w")) {
+					console.log(this.style.getPropertyValue("--w"))
+					let theAspectRatio=this.style.getPropertyValue("--w")*8+"/"+this.style.getPropertyValue("--h")*7; modalImg.style.objectFit="fill"
+					modal.style.aspectRatio=theAspectRatio
+					modalImg.style.aspectRatio=theAspectRatio
+					modalImg.style.height="initial"
+					modalImg.style.width="100%"
+				} else {modal.style.removeProperty('aspect-ratio'); modalImg.style.removeProperty('aspect-ratio'); modalImg.style.objectFit="contain"; modalImg.style.removeProperty('width'); modalImg.style.height="100%"}
+			} else {modal.style.imageRendering="auto"; modal.style.removeProperty('aspect-ratio'); modalImg.style.removeProperty('aspect-ratio'); modalImg.style.objectFit="contain"; modalImg.style.removeProperty('width'); modalImg.style.height="100%"}
 			modal.showModal()
+			modal.style.display="flex"
 		});
 		elem.addEventListener("keydown", function(e) {
 			if (e.key === "Enter") {
@@ -44,5 +55,5 @@ document.addEventListener('DOMContentLoaded', function() {
 			this.parentElement.style.removeProperty('border-color')
 		})
 	})
-	document.head.innerHTML+='<style>.gallery img{cursor:pointer}.gallery img,.gallery>div{transition-duration:.4s}.gallery>img:hover,.gallery>img:focus-visible{border-color:var(--pink)} #imgmodal img {width:80vw;max-height:80vh;background-color:white} dialog{background-color:white;border-radius:1rem} #imgmodal img[src$=".svg"]{padding:2vw} dialog::backdrop{background-color: rgb(255 255 255 / 50%)}</style>'
+	document.head.innerHTML+='<style>#imgmodal {height:80vh;align-items:center;justify-content:center} .gallery img{cursor:pointer}.gallery img,.gallery>div{transition-duration:.4s}.gallery>img:hover,.gallery>img:focus-visible{border-color:var(--pink)} #imgmodal img {background-color:white; height:100%} dialog{background-color:white;border-radius:1rem} #imgmodal img[src$=".svg"]{/*padding:2vw*/} dialog::backdrop{background-color: rgb(255 255 255 / 50%)}</style>'
 })
